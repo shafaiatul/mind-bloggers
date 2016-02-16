25e408d6ed86c4d7289877996d3543b669035dfb
Shafaiatul Islam <shafaiatul@gmail.com>
2016-02-15 22:58:04 -0500

    Prepare the Script and Data

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,26 @@
 (function(){
 
-	// script goes here
+	var app = angular.module('myQuiz', []);
+
+	//$http : This service allows to load external content, here the json file.
+	//$sce  : This servce allows to inject html from the my json file direclty to my html document 
+	app.controller('QuizCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+
+		//Initial State (Before the Quiz begins)
+		$scope.score = 0;
+		$scope.activeQuestion = -1; //I will show the "Into" screen if the value is -1,  and disappear it when more than -1
+		$scope.activeQuestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').then(function (quizData) {
+			$scope.myQuestions = quizData.data; // all questions in the json file
+			$scope.totalQuestions = $scope.myQuestions.length; //number of question in the json file
+		})
+
+		$scope.selectAnswer = function(qIndex, aIndex) {
+			alert (qIndex + ' and ' + aIndex);
+		}
+
+	}])
 
 })();
