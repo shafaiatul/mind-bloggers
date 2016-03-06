806587d5d230e4af5faf0450b6fcf43aa54653d4
Shafaiatul Islam <shafaiatul@gmail.com>
2016-03-06 15:10:57 -0500

    Add active answer state and add feedback

--- js/quiz.js
+++ js/quiz.js
@@ -1,9 +1,10 @@
 (function(){
+    'use strict';
 
 	var app = angular.module('myQuiz', []);
 
-	//$http : This service allows to load external content, here the json file.
-	//$sce  : This servce allows to inject html from the my json file direclty to my html document 
+	//$http : This service allows to load external content, here from the json file.
+	//$sce  : This servce allows to inject html from the my json file direclty to my html document
 	app.controller('QuizCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
 
 		//Initial State (Before the Quiz begins)
@@ -15,12 +16,39 @@
 		$http.get('quiz_data.json').then(function (quizData) {
 			$scope.myQuestions = quizData.data; // all questions in the json file
 			$scope.totalQuestions = $scope.myQuestions.length; //number of question in the json file
-		})
+		});
 
 		$scope.selectAnswer = function(qIndex, aIndex) {
-			alert (qIndex + ' and ' + aIndex);
-		}
+			//verify whether the question has been answered
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            if(questionState != 'answered') {
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex; //here I check the selected answer the user has clicked on
+                var correctAnswer = $scope.myQuestions[qIndex].correct; //correct answer from the JSON file
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                //Now I will check the answer clicked on matches the correct answer or not
+                if (aIndex === correctAnswer){
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                } else {
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+                //The reason I have to write this line of code because the user can still click on the individual answers
+                //Even I turned off the cursor and hover-state, ANGULAR is still paying attension to every one of the answers and user can still click on the answers
+                //But now after adding this line of code, after first answer selection, user can click but nothing will happen
+                $scope.myQuestions[qIndex].questionState = 'answered';
+            }
+		};
 
-	}])
+        $scope.isSelected = function (qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        };
+        $scope.isCorrect = function (qIndex, aIndex) {
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        };
+
+        $scope.selectContinue = function () {
+            return $scope.activeQuestion += 1;
+        };
+	}]);
 
 })();
